214c3103c06a6c485357a7bd79f1016cb4c231f8
Copons <copons@users.noreply.github.com>
2016-12-28 14:03:07 +0100

    Move search actions into SearchField

--- lib/search-field.jsx
+++ lib/search-field.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import appState from './flux/app-state';
+import { tracks } from './analytics';
 
 const KEY_ESC = 27;
 
@@ -51,10 +53,24 @@ export class SearchField extends Component {
 	}
 };
 
-const mapStateToProps = ( { appState } ) => ( {
-	query: appState.filter,
-	placeholder: appState.listTitle,
-	searchFocus: appState.searchFocus,
+const {
+	search,
+	setSearchFocus,
+} = appState.actionCreators;
+const { recordEvent } = tracks;
+
+const mapStateToProps = ( { appState: state } ) => ( {
+	query: state.filter,
+	placeholder: state.listTitle,
+	searchFocus: state.searchFocus,
 } );
 
-export default connect( mapStateToProps )( SearchField );
+const mapDispatchToProps = dispatch => ( {
+	onSearch: filter => {
+		dispatch( search( { filter } ) );
+		recordEvent( 'list_notes_searched' );
+	},
+	onSearchFocused: () => dispatch( setSearchFocus( { searchFocus: false } ) ),
+} );
+
+export default connect( mapStateToProps, mapDispatchToProps )( SearchField );
